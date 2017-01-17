a252d6943e73b3f28035483164a747ac7236625f
M.C. van den Boogaart <scola@kvdb.name>
2017-01-17 17:10:36 +0100

    Refactor

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import limit from './src/limit';
 import order from './src/order';
 import where from './src/where';
 
-export function extend(connection) {
-  connection.limit = limit;
-  connection.order = order;
-  connection.where = where;
-
-  return connection;
+export function extend(object) {
+  return Object.assign(object, {
+    limit,
+    order,
+    where
+  });
 }
